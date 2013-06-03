7398a8b36d512c74399e9a18aa77813171a3dab4
Robert Vella <robert.r.h.vella@gmail.com>
2013-06-03 22:57:39 +0200

    First commit of main sketch code.
    Agent controlled using arrow keys.
    No collision detection.

---
+++ src/asteroidsGame.js
@@ -0,0 +1,136 @@
+/*
+ Copyright (c) 2013, robert.r.h.vella@gmail.com
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ The views and conclusions contained in the software and documentation are those
+ of the authors and should not be interpreted as representing official policies,
+ either expressed or implied, of the FreeBSD Project.
+ */
+
+(function (asteroids) {
+    "use strict";
+
+    asteroids.AsteroidsGame = function (args) {
+        var self = this;
+
+        self.canvas = args.canvas;
+        self.drawingContext = self.canvas.getContext("2d");
+        self._keysDownDict = {};
+
+        self.gameObjects = [
+            new asteroids.AIControlledShip({
+                game: self,
+                position: new SAT.Vector(
+                    self.canvas.width * 0.5,
+                    self.canvas.height * 0.5
+                )
+            }),
+
+            new asteroids.StaticAsteroid({
+                game: self,
+                position: new SAT.Vector(
+                    self.canvas.width * 0.25,
+                    self.canvas.height * 0.25
+                )
+            }),
+
+            new asteroids.StaticAsteroid({
+                game: self,
+                position: new SAT.Vector(
+                    self.canvas.width * 0.75,
+                    self.canvas.height * 0.25
+                )
+            }),
+
+            new asteroids.StaticAsteroid({
+                game: self,
+                position: new SAT.Vector(
+                    self.canvas.width * 0.25,
+                    self.canvas.height * 0.75
+                )
+            }),
+
+            new asteroids.StaticAsteroid({
+                game: self,
+                position: new SAT.Vector(
+                    self.canvas.width * 0.75,
+                    self.canvas.height * 0.75
+                )
+            })
+        ];
+
+        $(window).keydown(function (event) {
+            self._recordKeyDown(event.which);
+        });
+
+        $(window).keyup(function (event) {
+            self._recordKeyUp(event.which);
+        });
+    };
+
+    asteroids.AsteroidsGame.prototype._recordKeyDown = function (code) {
+        var self = this;
+
+        self._keysDownDict[code] = true;
+    };
+
+    asteroids.AsteroidsGame.prototype._recordKeyUp = function (code) {
+        var self = this;
+
+        delete self._keysDownDict[code];
+    };
+
+    asteroids.AsteroidsGame.prototype.isKeyDown = function (code) {
+        var self = this;
+
+        return self._keysDownDict[code] === true;
+    };
+
+    asteroids.AsteroidsGame.prototype.draw = function () {
+        var self = this;
+
+        self.drawingContext.clearRect(0, 0, self.canvas.width, self.canvas.height);
+
+        _.each(self.gameObjects, function (gameObject) {
+            gameObject.draw();
+        });
+    };
+
+    asteroids.AsteroidsGame.prototype.update = function () {
+        var self = this;
+
+        _.each(self.gameObjects, function (gameObject) {
+            gameObject.update();
+        });
+    };
+
+    asteroids.AsteroidsGame.prototype.run = function () {
+        var self = this;
+
+        var framesPerSecond = 30;
+        var updatesPerSecond = 10;
+
+        setInterval(_.bind(self.draw, self), 1000.0 / framesPerSecond);
+        setInterval(_.bind(self.update, self), 1000.0 / updatesPerSecond);
+    };
+}(window.asteroids = window.asteroids || {}));
---
+++ src/asteroidsGameObjects.js
@@ -0,0 +1,166 @@
+/*
+ Copyright (c) 2013, robert.r.h.vella@gmail.com
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ The views and conclusions contained in the software and documentation are those
+ of the authors and should not be interpreted as representing official policies,
+ either expressed or implied, of the FreeBSD Project.
+ */
+
+
+(function (asteroids) {
+    "use strict";
+
+    asteroids.AsteroidsGameObject = function (args) {
+        var self = this;
+
+        self.game = args.game;
+
+        self.position = args.position;
+        self.rotation = args.rotation;
+
+        self.size = args.size;
+
+        self.image = new Image();
+        self.image.src = args.imageSrc;
+
+        self.velocity = new SAT.Vector(0, 0);
+        self.rotation = 0;
+    };
+
+    asteroids.AsteroidsGameObject.prototype.draw = function () {
+        var self = this;
+
+
+        self.game.drawingContext.drawImage(
+            self.image,
+            self.position.x - self.size / 2,
+            self.position.y - self.size / 2,
+            self.size,
+            self.size
+        );
+    };
+
+    asteroids.AsteroidsGameObject.prototype.update = function () {
+        //pass
+    };
+
+    asteroids.AIControlledShip = function (args) {
+        var self = this;
+
+        asteroids.AsteroidsGameObject.call(
+            self,
+            {
+                game: args.game,
+
+                position: args.position,
+                size: 32,
+
+                imageSrc: "media/ship.png"
+
+            }
+        );
+    };
+
+    _.extend(asteroids.AIControlledShip.prototype, asteroids.AsteroidsGameObject.prototype);
+    asteroids.AIControlledShip.prototype.constructor = asteroids.AIControlledShip;
+
+    asteroids.AIControlledShip.prototype.normalizeAngle = function () {
+        var self = this;
+        var twoPI = Math.PI * 2;
+
+        self.rotation = self.rotation % twoPI;
+
+        if(self.rotation < 0) {
+             self.rotation += twoPI;
+        }
+    };
+
+    asteroids.AIControlledShip.prototype.getHeading = function () {
+        var self = this;
+
+        self.normalizeAngle();
+
+        var result = new SAT.Vector(-Math.sin(self.rotation), Math.cos(self.rotation));
+        result.normalize();
+
+        return result;
+    };
+
+    asteroids.AIControlledShip.prototype.accelerate = function () {
+        var self = this;
+
+        var accelerationMagnitude = 10;
+        self.velocity.add(self.getHeading().scale(accelerationMagnitude));
+    };
+
+    asteroids.AIControlledShip.prototype.turn = function (direction) {
+        var self = this;
+
+        var rotationAmount = Math.PI * 0.05;
+        self.rotation += rotationAmount * direction;
+
+        self.normalizeAngle();
+    };
+
+    asteroids.AIControlledShip.prototype.update = function () {
+        var self = this;
+
+        var dampeningFactor = 0.5;
+        self.velocity.scale(dampeningFactor);
+
+        if (self.game.isKeyDown(asteroids.KeyCodes.UP)) {
+            self.accelerate();
+        }
+
+        if (self.game.isKeyDown(asteroids.KeyCodes.RIGHT)) {
+            self.turn(1);
+        } else if (self.game.isKeyDown(asteroids.KeyCodes.LEFT)) {
+            self.turn(-1);
+        }
+
+        self.position.add(self.velocity);
+
+        asteroids.AsteroidsGameObject.prototype.update.call(self);
+    };
+
+    asteroids.StaticAsteroid = function (args) {
+        var self = this;
+
+        asteroids.AsteroidsGameObject.call(
+            self,
+            {
+                game: args.game,
+
+                position: args.position,
+                size: 128,
+
+                imageSrc: "media/asteroid.png"
+
+            }
+        );
+    };
+
+    _.extend(asteroids.StaticAsteroid.prototype, asteroids.AsteroidsGameObject.prototype);
+    asteroids.StaticAsteroid.prototype.constructor = asteroids.StaticAsteroid;
+}(window.asteroids = window.asteroids || {}));
